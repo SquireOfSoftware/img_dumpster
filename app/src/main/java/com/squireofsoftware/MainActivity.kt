45a78f59b083bd8dc8efb4d05e28dc8f28ab96ff
SquireOfSoftware <fred.smoth@gmail.com>
2023-04-07 14:58:09 +1000

    Getting chatgpt to help me write up the android app
    - This current iteration allows the app to read the photos on the android phone

--- app/src/main/java/com/squireofsoftware/MainActivity.kt
+++ app/src/main/java/com/squireofsoftware/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.squireofsoftware
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.squireofsoftware.ui.theme.ImgTrashCanTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,25 +24,45 @@ class MainActivity : ComponentActivity() {
             ImgTrashCanTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    CurrentDirectory("Mum", from = "John")
+
                 }
             }
         }
-    }
-}
 
-@Composable
-fun CurrentDirectory(message: String, from: String = "", modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Hi $message",
-            fontSize = 36.sp
-        )
-        Text(
-            text = "~ $from",
-            fontSize = 24.sp
-        )
+        readPhotosFromDevice()
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted, read the photos
+                readPhotosFromDevice()
+            } else {
+                // Permission is denied, show a message or do something else
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun readPhotosFromDevice() {
+        if (ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            requestPermissionLauncher.launch(READ_EXTERNAL_STORAGE)
+            return
+        }
+        // Permission is already granted, read the photos
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, null)
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val imagePath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+                // Display a text popup of the image path
+                Toast.makeText(this, "Image path: $imagePath", Toast.LENGTH_SHORT).show()
+            }
+            cursor.close()
+        }
+    }
+
 }
 
 @Preview(showBackground = false,
@@ -48,6 +71,6 @@ fun CurrentDirectory(message: String, from: String = "", modifier: Modifier = Mo
 @Composable
 fun TrashCanPreview() {
     ImgTrashCanTheme {
-        CurrentDirectory(message = "Mum", from = "John")
+
     }
 }
